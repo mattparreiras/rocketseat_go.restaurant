92c0a346cb467f996d2574ee2b6e69ec2a22f4ce
mpsantos8 <matheus.masan@gmail.com>
2021-04-16 00:11:44 -0300

    Convert input to a ts component and install @types/react-icons

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,15 +2,22 @@ import {
   useEffect,
   useRef,
   useState,
-  useCallback,
+  useCallback
 } from 'react';
 
 import { useField } from '@unform/core';
-
+import {IconType} from 'react-icons';
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+
+interface Props {
+  name:string,
+  icon?: IconType
+}
+type InputProps = JSX.IntrinsicElements['input'] & Props
+
+const Input = ({ name, icon: Icon, ...rest }:InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -24,7 +31,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
 
-    setIsFilled(!!inputRef.current?.value);
+    setIsFilled(!!inputRef.current?.value  );
   }, []);
 
   useEffect(() => {
